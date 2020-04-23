d77195350254bd1281b322f6f3ab56bcb2aac0e3
d3jw <dejw.sedlar@gmail.com>
2020-04-23 18:12:35 +0200

    text atoms

---
+++ src/components/atoms/Headlines.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const H1 = styled.h1`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;
+export const H2 = styled.h2`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;
+export const H3 = styled.h3`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;
+export const H4 = styled.h4`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;
---
+++ src/components/atoms/TextFields.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const P = styled.p`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;
+
+export const Label = styled.label`
+  color: ${(props) => props.theme.colors.primaryDark};
+`;

726ee457e81433a42e1cb53aa2802afaeb0f7068
d3jw <dejw.sedlar@gmail.com>
2020-04-23 17:50:48 +0200

    theme added

---
+++ src/styles/Theme.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { ThemeProvider } from "emotion-theming";
+
+const theme = {
+  colors: {
+    primary: "#F2C12E",
+    primaryLight: "#F2C46D",
+    primaryDark: "#94753a",
+    accent: "#ADD96C",
+    background: "#D9A689",
+    white: "#ffffff",
+  },
+};
+
+const Theme = ({ children }) => {
+  console.log(theme);
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Theme;
